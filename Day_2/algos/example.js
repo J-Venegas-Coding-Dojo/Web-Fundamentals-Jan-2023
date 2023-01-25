94520a778942915cba5a9d73c8a78fcc4a2ccade
J-Venegas-Coding-Dojo <jvenegas@codingdojo.com>
2023-01-24 23:59:30 -0600

    Day 2 Commit

---
+++ Day_2/algos/example.js
@@ -0,0 +1,76 @@
+//What is a variable?
+
+
+//a container that holds a value, a box that can change value/name or
+//content
+
+
+//They can be reused
+//They can vary
+//They need labels
+//Labels, they need to describe the contents of the box
+//Do labels matter to the computer? no
+
+//In order to make a variable in JavaScript, we need the "var" keyword
+
+//keyword cannot be "var"
+
+var name = "josh"
+
+var instructor = "Christian"
+
+var firstName = "Heidi"
+
+
+
+
+
+//The console (in the terminal) is a place where you can print variables, and other stuff
+
+
+
+
+// console.log(lastName)
+
+//In JS we use camelCase - first word is lower case and the foloowing words are capitalized
+
+
+
+//What is data?
+
+// information that has value
+
+//What types of data can we use in JavaScript
+
+//1. String - "Smith" "Hello my name is Smith"
+
+//2. Number - 1,2,3,4 12312434523423
+
+//3. Boolean - has a true or false value
+
+//var test = false
+
+
+
+
+
+//What is an algorithm?
+
+//An algorithm is a set of instructions
+//A set of instructions to solve a problem given different circumstances
+//i stands for index
+//This below is a "FOR LOOP"
+
+for(var i=0; i< 10; i++){
+
+    console.log(i)
+}
+var lastName = "Smith"
+//one equal sign is assigning the value
+
+//two equal signs is a value check, esentailly, it's saying "is it equal to"
+
+if (lastName == "Smith"){
+    //do something
+    console.log("hello")
+}
